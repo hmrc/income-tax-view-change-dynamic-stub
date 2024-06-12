1022f4463fc8f1bd954f0306c219fca5ed0cc39d
Daniel Maltz <117353102+danielmaltz1@users.noreply.github.com>
2024-06-12 10:11:05 +0100

    MISUV-7394 Dymanic data for poa amount (#87)
    * Added working intercept and setting of new data
    
    * Added dynamic creation on chargeRef and 1554 data
    
    * Moved methods to a utils file, temporarily disabled 1554 code
    
    * Added handling and interception for 1553 call
    
    * Tidy up
    
    * Removed unused utils methods
    
    * removed conversion to Int

--- app/controllers/SubmitPoaController.scala
+++ app/controllers/SubmitPoaController.scala
@@ -16,24 +16,31 @@
 
 package controllers
 
-import play.api.{Configuration, Logger, Logging}
+import models.TaxYear
+import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.result
 import play.api.libs.json.JsValue
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.{Configuration, Logger, Logging}
+import repositories.DataRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import utils.PoaUtils
 
 import java.net.URI
 import javax.inject.Inject
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class SubmitPoaController @Inject()(cc: MessagesControllerComponents,
                                     requestHandlerController: RequestHandlerController,
-                                    configuration: Configuration
-                                   ) extends FrontendController(cc) with Logging {
+                                    configuration: Configuration,
+                                    dataRepository: DataRepository
+                                   )(implicit val ec: ExecutionContext)
+  extends FrontendController(cc) with Logging with PoaUtils {
 
-  val error1773Ninos: Seq[String] = configuration.getOptional[Seq[String]]("api1773ErrorResponseNinos")
+  private val error1773Ninos: Seq[String] = configuration.getOptional[Seq[String]]("api1773ErrorResponseNinos")
     .getOrElse(Seq.empty)
 
-  def interceptSubmitForErrorUser(): Action[AnyContent] = Action.async { implicit request =>
+  def interceptPoaSubmit(): Action[AnyContent] = Action.async { implicit request =>
 
     val bodyOption: Option[JsValue] = request.body.asJson
     val InvalidJsonError = BadRequest("Invalid JSON in the UpdateIncomeSourceRequest body")
@@ -50,6 +57,8 @@ class SubmitPoaController @Inject()(cc: MessagesControllerComponents,
                 Logger("application").info(s"Returning stubbed API#1773 error response for nino: $nino")
                 request.withTarget(request.target.withUri(URI.create(request.uri + s"?nino=$nino")))
               } else {
+                //Replace poa amount with new amount
+                overwriteTotalAmount(nino, body)
                 // Retrieve stubbed success response from ATs
                 Logger("application").info(s"Returning stubbed API#1773 success response for nino: $nino")
                 request
@@ -61,7 +70,34 @@ class SubmitPoaController @Inject()(cc: MessagesControllerComponents,
     }
   }
 
-  def extractNino(json: JsValue): Option[String] = {
-    (json \ "nino").asOpt[String]
+  private def overwriteTotalAmount(nino: String, json: JsValue): Unit = {
+    (extractPoAAmount(json), extractTaxYear(json)) match {
+      case (Some(amount), Some(taxYearString)) =>
+        TaxYear.createTaxYearGivenTaxYearRange(taxYearString) match {
+          case Some(taxYear) =>
+            val financialUrl = getFinancialDetailsUrl(nino, taxYear)
+            val financialDetailsResponse = dataRepository.find(equal("_id", financialUrl))
+            financialDetailsResponse.map {
+              case Some(value) => value.response match {
+                case Some(response) =>
+                  performDataChanges(response, amount, financialUrl)
+                  Logger("application").info(s"Overwrote totalAmount data for $nino with new amount $amount")
+                case None =>
+                  Future.failed(new Exception("Could not find response in financial details 1553 data for this nino"))
+              }
+              case None => Future.failed(new Exception("Could not find financial details 1553 data for this nino"))
+            }
+          case None => Future.failed(new Exception("Failed to create tax year from request"))
+        }
+      case _ => Future.failed(new Exception("Could not extract poa amount or tax year from request"))
+    }
   }
+
+  private def performDataChanges(response: JsValue, amount: BigDecimal, financialUrl: String): Future[result.UpdateResult] = {
+    //Create new 1553 data with totalAmount overwritten with new poa amount
+    val newResponse = response.transform(transformDocDetails(amount)).getOrElse(response)
+    //Overwrite existing 1553 data with the new poa amount
+    dataRepository.replaceOne(url = financialUrl, updatedFile = getFinDetailsDataModel(newResponse, financialUrl))
+  }
+
 }
---
+++ app/utils/PoaUtils.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import models.{DataModel, TaxYear}
+import play.api.libs.json.Reads.of
+import play.api.libs.json.{JsArray, JsNumber, JsObject, JsValue, Reads, __}
+
+trait PoaUtils {
+
+  def extractNino(json: JsValue): Option[String] = {
+    (json \ "nino").asOpt[String]
+  }
+
+  def extractPoAAmount(json: JsValue): Option[BigDecimal] = {
+    (json \ "amount").asOpt[BigDecimal]
+  }
+
+  def extractTaxYear(json: JsValue): Option[String] = {
+    (json \ "taxYear").asOpt[String]
+  }
+
+  def getFinancialDetailsUrl(nino: String, taxYear: TaxYear): String = {
+    s"/enterprise/02.00.00/financial-data/NINO/$nino/ITSA?dateFrom=${taxYear.startYear}-04-06&dateTo=${taxYear.endYear}-04-05&onlyOpenItems=false&includeLocks=true&calculateAccruedInterest=true&removePOA=false&customerPaymentInformation=true&includeStatistical=false&afterPoaAmountAdjusted=true"
+  }
+
+  def transformDocDetails(amount: BigDecimal): Reads[JsObject] = {
+    (__ \ "documentDetails").json.update(
+      of[JsArray].map {
+        case JsArray(arr) =>
+          JsArray(arr.map(item => item.transform(transformAmount(amount)).getOrElse(item)))
+      }
+    )
+  }
+
+  def transformAmount(amount: BigDecimal): Reads[JsObject] = {
+    (__ \ "totalAmount").json.update(
+      of[JsNumber].map {
+        case JsNumber(_) =>
+          JsNumber(amount)
+      }
+    )
+  }
+
+  def getFinDetailsDataModel(request: JsValue, url: String): DataModel = DataModel(
+    _id = url,
+    schemaId = "getFinancialDetailsSuccess",
+    method = "GET",
+    status = 200,
+    response = Some(request)
+  )
+}
