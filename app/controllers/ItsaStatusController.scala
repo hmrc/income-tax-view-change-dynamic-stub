08a330a8320561c389e47d56266af11cb99664d7
Christine Stokes <18244373+Chrissystk@users.noreply.github.com>
2025-05-02 15:31:38 +0100

    MISUV-9620 added hip endpoint for itsa status and updated override (#117)
    * MISUV-9620 added hip endpoint for itsa status and updated override itsa status to include new hip api
    
    * MISUV-9620 updated the stub route
    
    * MISUV-9614 fixing mistake in HIP url for override ITSA status
    
    * MISUV-9614 fixing mistake in HIP url for override ITSA status

--- app/controllers/ItsaStatusController.scala
+++ app/controllers/ItsaStatusController.scala
@@ -16,10 +16,12 @@
 
 package controllers
 
-import models.{ItsaStatus, TaxYear}
+import models.HttpMethod.GET
+import models.{ItsaStatus, Nino, TaxYear}
+import org.mongodb.scala.model.Filters.equal
 import play.api.{Logger, Logging}
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import repositories.DataRepository
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import repositories.{DataRepository, DefaultValues}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 import javax.inject.{Inject, Singleton}
@@ -28,7 +30,8 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class ItsaStatusController @Inject() (
     cc:             MessagesControllerComponents,
-    dataRepository: DataRepository
+    dataRepository: DataRepository,
+    defaultValues:  DefaultValues
   )(
     implicit val ec: ExecutionContext)
     extends FrontendController(cc)
@@ -38,6 +41,28 @@ class ItsaStatusController @Inject() (
     s"/income-tax/$nino/person-itd/itsa-status/${taxYear.rangeShort}?futureYears=false&history=false"
   }
 
+  private def createOverwriteHipItsaStatusUrl(nino: String, taxYear: TaxYear): String = {
+    s"/itsd/person-itd/itsa-status/$nino?taxYear=${taxYear.rangeShort}&futureYears=false&history=false"
+  }
+
+  def getHIPITSAStatus(
+      nino:         String,
+      taxYearRange: String,
+      futureYears:  Boolean,
+      history:      Boolean
+    ): Action[AnyContent] = {
+    Action.async { implicit request =>
+      dataRepository.find(equal("_id", request.uri), equal("method", GET)).map { stubData =>
+        stubData.headOption match {
+          case Some(datamodel) if datamodel.response.nonEmpty =>
+            Status(datamodel.status)(stubData.head.response.get)
+          case Some(dataModel) => Status(dataModel.status)
+          case _               => Status(OK)(defaultValues.getHipItsaStatusDefaultJson(taxYearRange))
+        }
+      }
+    }
+  }
+
   def overwriteItsaStatus(nino: String, taxYearRange: String, itsaStatus: String): Action[AnyContent] =
     Action.async { _ =>
       Logger("application").info(
@@ -45,32 +70,7 @@ class ItsaStatusController @Inject() (
       )
 
       TaxYear.createTaxYearGivenTaxYearRange(taxYearRange) match {
-        case Some(taxYear: TaxYear) =>
-          val url           = createOverwriteItsaStatusUrl(nino = nino, taxYear = taxYear)
-          val itsaStatusObj = ItsaStatus(itsaStatus, url, taxYear)
-
-          dataRepository
-            .replaceOne(url = url, updatedFile = itsaStatusObj.makeOverwriteDataModel)
-            .map { result =>
-              if (result.wasAcknowledged) {
-                Logger("application").info(
-                  s"[ItsaStatusController][overwriteItsaStatus] Overwrite success! For < url: $url >"
-                )
-                Ok(s"Overwrite success! For < url: $url >")
-              } else {
-                Logger("application").info(
-                  s"[ItsaStatusController][overwriteItsaStatus] Write was not acknowledged! For < url: $url >"
-                )
-                InternalServerError("Write was not acknowledged")
-              }
-            }
-            .recoverWith {
-              case ex =>
-                Logger("application").error(
-                  s"[ItsaStatusController][overwriteItsaStatus] Update operation failed. < Exception: $ex >"
-                )
-                Future.failed(ex)
-            }
+        case Some(taxYear: TaxYear) => overrideItsaStatusForTaxYear(taxYear, nino, itsaStatus)
         case None =>
           Logger("application").error(
             s"[ItsaStatusController][overwriteItsaStatus] taxYearRange could not be converted to TaxYear"
@@ -78,5 +78,49 @@ class ItsaStatusController @Inject() (
           Future.failed(new Exception("taxYearRange could not be converted to TaxYear"))
       }
     }
+
+  private def overrideItsaStatusForTaxYear(taxYear: TaxYear, nino: String, itsaStatus: String): Future[Result] = {
+    val url              = createOverwriteItsaStatusUrl(nino = nino, taxYear = taxYear)
+    val hipUrl           = createOverwriteHipItsaStatusUrl(nino = nino, taxYear = taxYear)
+    val itsaStatusObj    = ItsaStatus(itsaStatus, url, taxYear, false)
+    val itsaStatusHipObj = ItsaStatus(itsaStatus, hipUrl, taxYear, true)
+
+    val res = for {
+      ifStatusOverride <-
+        dataRepository
+          .replaceOne(url = url, updatedFile = itsaStatusObj.makeOverwriteDataModel)
+      hipStatusOverride <-
+        dataRepository
+          .replaceOne(url = hipUrl, updatedFile = itsaStatusHipObj.makeOverwriteDataModel)
+    } yield {
+      (ifStatusOverride.wasAcknowledged(), hipStatusOverride.wasAcknowledged()) match {
+        case (true, true) => Some(s"For < url: $url and $hipUrl>")
+        case (true, true) => Some(s"For < url: $url>")
+        case (true, true) => Some(s"For < url: $hipUrl>")
+        case _            => None
+      }
+    }
+
+    res
+      .map {
+        case Some(urlsUpdated) =>
+          Logger("application").info(
+            s"[ItsaStatusController][overwriteItsaStatus] Overwrite success! $urlsUpdated "
+          )
+          Ok(s"Overwrite success! $urlsUpdated")
+        case _ =>
+          Logger("application").info(
+            s"[ItsaStatusController][overwriteItsaStatus] Write was not acknowledged! For < url: $url or $hipUrl>"
+          )
+          InternalServerError("Write was not acknowledged")
+      }
+      .recoverWith {
+        case ex =>
+          Logger("application").error(
+            s"[ItsaStatusController][overwriteItsaStatus] Update operation failed. < Exception: $ex >"
+          )
+          Future.failed(ex)
+      }
+  }
 
 }
